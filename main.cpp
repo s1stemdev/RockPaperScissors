2c12823c5ccc2f18a511863e7b5b928a860e3982
s1stemdev <108710611+s1stemdev@users.noreply.github.com>
2024-02-06 21:56:16 +0500

    ru version

--- main.cpp
+++ main.cpp
@@ -3,31 +3,31 @@
 
 using namespace std;
 
-string variants[3] = { "rock", "paper", "scissors" };
+string variants[3] = { "камень", "бумага", "ножницы" };
 
 random_device dev;
 uniform_int_distribution<int> dist(0, 2);
 
 string get_result(string player, string computer) {
 
-    if(player == computer) return "Draw";
-    if(player == variants[2] && computer == variants[1]) return "You win!";
-    if(player == variants[1] && computer == variants[0]) return "You win!";
-    if(player == variants[0] && computer == variants[2]) return "You win!";
+    if(player == computer) return "Ничья";
+    if(player == variants[2] && computer == variants[1]) return "Вы выиграли!";
+    if(player == variants[1] && computer == variants[0]) return "Вы выиграли!";
+    if(player == variants[0] && computer == variants[2]) return "Вы выиграли!";
 
-    else return "You lose!";
+    else return "Вы проиграли!";
 }
 
 int main() {
     setlocale(LC_ALL, "Rus");
 
     while (true) {
-        cout << "Your move! (rock, paper, scissors) ''cancel'' to exit: ";
+        cout << "Ваш ход! (камень, бумага, ножницы) ''выход'' для выхода: ";
 
         string player_move;
         cin >> player_move;
 
-        if(player_move == "cancel") return 1;
+        if(player_move == "выход") return 1;
 
         string computer_move = variants[dist(dev)];
         cout << computer_move << endl;
